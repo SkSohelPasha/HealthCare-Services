3d5c7b5a525489d7a05c12a429fc3dd37b53d3ab
SkSohelPasha <sohelpasha019@gmail.com>
2025-08-22 21:09:08 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CartProvider } from "@/contexts/CartContext";
 import HomePage from "./pages/HomePage";
@@ -22,7 +26,12 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter
+            future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true,
+            }}
+          >
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/packages" element={<PackagesPage />} />
